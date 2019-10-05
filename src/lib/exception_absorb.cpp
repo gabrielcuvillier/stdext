b33ea78b52b1e3a0966891e039364ce48ab4a8d5
Gabriel <gabriel.cuvillier@continuation-labs.com>
2019-10-05 23:11:28 +0200

    prototype 'exception_absorber'

---
+++ src/lib/exception_absorb.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 - Gabriel Cuvillier, Continuation Labs (www.continuation-labs.com)
+// Licensed under the MIT License.
+
+// Main header
+#include <stdext/exception_absorb>
+
+// std
+#include <string>
+#include <iostream>
+#include <exception>
+#include <typeinfo>
+
+stdext::_ExceptionAbsorber::_ExceptionAbsorber(std::exception const & err) noexcept :
+    type_(typeid(err).name()),
+    message_(err.what()) { }
+
+stdext::_ExceptionAbsorber::~_ExceptionAbsorber() noexcept {
+  std::cerr << type_ << ": " << message_ << std::endl;
+  std::terminate(); // Ideally, std::set_terminate should be set by the main application
+};
