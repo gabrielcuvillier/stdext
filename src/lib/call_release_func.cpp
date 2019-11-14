15eb27065f46119bff5e68a70f35ab16d501ee7a
Gabriel <gabriel.cuvillier@continuation-labs.com>
2019-11-14 11:50:46 +0100

    refactor

--- src/lib/call_release_func.cpp
+++ src/lib/call_release_func.cpp
@@ -4,21 +4,20 @@
 // Main header
 #include <stdext/call_release_func>
 
-stdext::call_release_func::call_release_func(void(*ipFunc)()) noexcept : _pFunc(ipFunc) {
-}
+stdext::call_release_func::call_release_func( void ( *ipFunc )() ) noexcept : _pFunc( ipFunc ) {}
 
-stdext::call_release_func::call_release_func(call_release_func &&iMoveFunc) noexcept : _pFunc(iMoveFunc._pFunc) {
+stdext::call_release_func::call_release_func( call_release_func&& iMoveFunc ) noexcept : _pFunc( iMoveFunc._pFunc ) {
   iMoveFunc._pFunc = nullptr;
 }
 
 stdext::call_release_func::~call_release_func() {
-  if (_pFunc) {
+  if ( _pFunc ) {
     _pFunc();
     _pFunc = nullptr;
   }
 }
 
-stdext::call_release_func &stdext::call_release_func::operator=(call_release_func &&iMoveFunc) noexcept {
+stdext::call_release_func& stdext::call_release_func::operator=( call_release_func&& iMoveFunc ) noexcept {
   _pFunc = iMoveFunc._pFunc;
   iMoveFunc._pFunc = nullptr;
   return *this;
--- src/lib/error.cpp
+++ src/lib/error.cpp
@@ -5,53 +5,78 @@
 #include <stdext/error>
 
 // STD
+#include <cstdio>     // std::fprintf, stdout, stderr
+#include <cstring>    // std::strcpy, std::strlen
 #include <cstdlib>    // std::abort
-#include <cstdio>     // std::printf
-#include <iostream>   // std::cerr, std::endl
-#include <string>     // std::string
 #include <exception>  // std::set_terminate
-#if !defined(__GNUC__)||defined(__clang__)
-#include <filesystem> // std::filesystem
+#include <string>     // std::string
+#if !defined(_WIN32)
+#include <libgen.h>   // basename
 #endif
 
 void stdext::install_unhandled_exception_handler() {
-  std::set_terminate([]() {
-    std::cerr << "[stdext] Unhandled exception. Aborting the program." << std::endl;
+  std::set_terminate( []() {
+    std::fprintf( stderr, "Terminate handler: aborting the program.");
     std::abort();
-  });
+  } );
 }
 
 template<>
-stdext_NODISCARD std::string stdext::enum_to_string(stdext::InternalError err) {
-  switch (err) {
-    case stdext::InternalError::AssertionFailed: return std::string{"InternalError::AssertionFailed"};
-    default: return std::string{};
+stdext_NODISCARD const char* stdext::enum_to_string( stdext::InternalError err ) {
+  switch ( err ) {
+    case stdext::InternalError::AssertionFailed:
+      return "InternalError::AssertionFailed";
+    default:
+      return nullptr;
   }
 }
 
-void stdext::PRINTERROR(const char *file,
-                        int line,
-                        const char *func,
-                        const char *message) {
-  std::printf("Failure at %s:%d: function ‘%s‘: '%s'",
-#if !defined(__GNUC__)||defined(__clang__)
-              std::filesystem::path(file).filename().c_str(),
+void stdext::PRINTERROR( const char* const file, const int line, const char* const func, const char* const message ) {
+  char * file_copy = nullptr;
+  if (file) {
+    const auto sz = std::strlen(file);
+    file_copy = new char[sz+1]; file_copy[sz] = 0;
+    std::strcpy(file_copy, file);
+  }
+  std::fprintf(stdout, "Failure at %s:%d: function ‘%s‘: '%s'",
+#if !defined(_WIN32)
+               basename(file_copy),
 #else
-              file,
+               file_copy,
 #endif
-              line,
-              func,
-              message);
+               line, func, message );
+  if (file_copy) {
+    delete [] file_copy;
+  }
 }
 
-void stdext::EPICFAIL_RET() { std::printf(" Aborting function\n"); }
+namespace {
+    const char* const abort_msg = " Aborting function\n";
+    const char* const skipping_iteration_msg = " Skipping iteration\n";
+    const char* const abort_loop_msg = " Aborting loop\n";
+    const char* const continue_msg = " Continuing\n";
+}
 
-void stdext::EPICFAIL_RET_VOID() { std::printf(" Aborting function\n"); }
+void stdext::EPICFAIL_RET() {
+  std::fprintf( stdout, abort_msg );
+}
 
-void stdext::EPICFAIL_RET_INT() { std::printf(" Aborting function\n"); }
+void stdext::EPICFAIL_RET_VOID() {
+  std::fprintf( stdout, abort_msg );
+}
 
-void stdext::EPICFAIL_LOOP() { std::printf(" Skipping iteration\n"); }
+void stdext::EPICFAIL_RET_INT() {
+  std::fprintf( stdout, abort_msg );
+}
 
-void stdext::EPICFAIL_LOOP_BREAK() { std::printf(" Aborting loop\n"); }
+void stdext::EPICFAIL_LOOP() {
+  std::fprintf( stdout, skipping_iteration_msg );
+}
 
-void stdext::EPICFAIL_NOP() { std::printf(" Continuing\n"); }
+void stdext::EPICFAIL_LOOP_BREAK() {
+  std::printf( abort_loop_msg );
+}
+
+void stdext::EPICFAIL_NOP() {
+  std::printf( continue_msg );
+}
