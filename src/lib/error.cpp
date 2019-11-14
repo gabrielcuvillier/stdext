263176526af6c0a4dc13b1d813186d853625fdda
Gabriel <gabriel.cuvillier@continuation-labs.com>
2019-11-14 11:54:49 +0100

    Formatting

--- src/lib/error.cpp
+++ src/lib/error.cpp
@@ -6,77 +6,68 @@
 
 // STD
 #include <cstdio>     // std::fprintf, stdout, stderr
-#include <cstring>    // std::strcpy, std::strlen
 #include <cstdlib>    // std::abort
+#include <cstring>    // std::strcpy, std::strlen
 #include <exception>  // std::set_terminate
 #include <string>     // std::string
-#if !defined(_WIN32)
-#include <libgen.h>   // basename
+#if !defined( _WIN32 )
+#include <libgen.h>  // basename
 #endif
 
-void stdext::install_unhandled_exception_handler() {
+void stdext::install_unhandled_exception_handler()
+{
   std::set_terminate( []() {
-    std::fprintf( stderr, "Terminate handler: aborting the program.");
+    std::fprintf( stderr, "Terminate handler: aborting the program." );
     std::abort();
   } );
 }
 
 template<>
-stdext_NODISCARD const char* stdext::enum_to_string( stdext::InternalError err ) {
+stdext_NODISCARD const char* stdext::enum_to_string( stdext::InternalError err )
+{
   switch ( err ) {
-    case stdext::InternalError::AssertionFailed:
-      return "InternalError::AssertionFailed";
-    default:
-      return nullptr;
+    case stdext::InternalError::AssertionFailed: return "InternalError::AssertionFailed";
+    default: return nullptr;
   }
 }
 
-void stdext::PRINTERROR( const char* const file, const int line, const char* const func, const char* const message ) {
-  char * file_copy = nullptr;
-  if (file) {
-    const auto sz = std::strlen(file);
-    file_copy = new char[sz+1]; file_copy[sz] = 0;
-    std::strcpy(file_copy, file);
+void stdext::PRINTERROR( const char* const file, const int line, const char* const func, const char* const message )
+{
+  char* file_copy = nullptr;
+  if ( file ) {
+    const auto sz = std::strlen( file );
+    file_copy = new char[sz + 1];
+    file_copy[sz] = 0;
+    std::strcpy( file_copy, file );
   }
-  std::fprintf(stdout, "Failure at %s:%d: function ‘%s‘: '%s'",
-#if !defined(_WIN32)
-               basename(file_copy),
+  std::fprintf( stdout, "Failure at %s:%d: function ‘%s‘: '%s'",
+#if !defined( _WIN32 )
+                basename( file_copy ),
 #else
-               file_copy,
+                file_copy,
 #endif
-               line, func, message );
-  if (file_copy) {
-    delete [] file_copy;
+                line, func, message );
+  if ( file_copy ) {
+    delete[] file_copy;
   }
 }
 
-namespace {
-    const char* const abort_msg = " Aborting function\n";
-    const char* const skipping_iteration_msg = " Skipping iteration\n";
-    const char* const abort_loop_msg = " Aborting loop\n";
-    const char* const continue_msg = " Continuing\n";
-}
+namespace
+{
+const char* const abort_msg = " Aborting function\n";
+const char* const skipping_iteration_msg = " Skipping iteration\n";
+const char* const abort_loop_msg = " Aborting loop\n";
+const char* const continue_msg = " Continuing\n";
+}  // namespace
 
-void stdext::EPICFAIL_RET() {
-  std::fprintf( stdout, abort_msg );
-}
+void stdext::EPICFAIL_RET() { std::fprintf( stdout, abort_msg ); }
 
-void stdext::EPICFAIL_RET_VOID() {
-  std::fprintf( stdout, abort_msg );
-}
+void stdext::EPICFAIL_RET_VOID() { std::fprintf( stdout, abort_msg ); }
 
-void stdext::EPICFAIL_RET_INT() {
-  std::fprintf( stdout, abort_msg );
-}
+void stdext::EPICFAIL_RET_INT() { std::fprintf( stdout, abort_msg ); }
 
-void stdext::EPICFAIL_LOOP() {
-  std::fprintf( stdout, skipping_iteration_msg );
-}
+void stdext::EPICFAIL_LOOP() { std::fprintf( stdout, skipping_iteration_msg ); }
 
-void stdext::EPICFAIL_LOOP_BREAK() {
-  std::printf( abort_loop_msg );
-}
+void stdext::EPICFAIL_LOOP_BREAK() { std::printf( abort_loop_msg ); }
 
-void stdext::EPICFAIL_NOP() {
-  std::printf( continue_msg );
-}
+void stdext::EPICFAIL_NOP() { std::printf( continue_msg ); }
--- src/lib/exception_absorb.cpp
+++ src/lib/exception_absorb.cpp
@@ -5,16 +5,18 @@
 #include <stdext/exception_absorb>
 
 // std
-#include <string>
-#include <iostream>
 #include <exception>
+#include <iostream>
+#include <string>
 #include <typeinfo>
 
-stdext::_ExceptionAbsorber::_ExceptionAbsorber(std::exception const & err) noexcept :
-    type_(typeid(err).name()),
-    message_(err.what()) { }
+stdext::_ExceptionAbsorber::_ExceptionAbsorber( std::exception const& err ) noexcept
+    : type_( typeid( err ).name() ), message_( err.what() )
+{
+}
 
-stdext::_ExceptionAbsorber::~_ExceptionAbsorber() noexcept {
+stdext::_ExceptionAbsorber::~_ExceptionAbsorber() noexcept
+{
   std::cerr << type_ << ": " << message_ << std::endl;
-  std::terminate(); // Ideally, std::set_terminate should be set by the main application
+  std::terminate();  // Ideally, std::set_terminate should be set by the main application
 };
