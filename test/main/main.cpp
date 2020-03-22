3a5346deab9f3fd86aa66384222792313daa7524
Gabriel Cuvillier <contact@gabrielcuvillier.pro>
2020-03-22 16:35:27 +0100

    removed [[maybe_unused]]

--- test/main/main.cpp
+++ test/main/main.cpp
@@ -7,15 +7,17 @@
 
 std::error_condition test_func() { return std::error_condition(); }
 
-int main( [[maybe_unused]] int argc, [[maybe_unused]] const char** argv ) noexcept
+int main( int argc, const char** argv ) noexcept
 {
   stdext::install_unhandled_exception_handler();
 
   ASSERT_RET_ONE( test_func() )
 
   std::cout << stdext::get_current_directory() << std::endl;
-  std::cout << stdext::get_file_name( argv[0] ) << std::endl;
-  std::cout << stdext::get_directory_name( argv[0] ) << std::endl;
+  if (argc > 0) {
+    std::cout << stdext::get_file_name( argv[0] ) << std::endl;
+    std::cout << stdext::get_directory_name( argv[0] ) << std::endl;
+  }
 
   return 0;
 }
